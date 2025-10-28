501c4b821e64dce5553e801d541c21b3ccf3accc
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-10-28 14:21:19 +0530

    updated client url

--- frontend/restaurant-client/src/api/analyticsService.js
+++ frontend/restaurant-client/src/api/analyticsService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 // functions to fetch the main dashboad analytics data
